ffc86feedf9a683f0b3382035c1e315558c9524c
Bernardo Koron <bernardokoron@gmail.com>
2025-04-06 14:18:24 +0200

    login endpoint

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from database import SessionLocal, User
@@ -43,5 +43,26 @@ def get_user_id(username: str, db: Session = Depends(get_db)):
     """
     user = db.query(User).filter(User.username == username).first()
     if not user:
-        raise HTTPException(status_code=404, detail="User not found")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return {"user_id": user.id}
+
+
+# Get user id
+@router.post("/log_in/", tags=["Authentication"])
+def get_user_id(credentials: dict, db: Session = Depends(get_db)):
+    """
+    Log in
+
+    - **username**: The username
+    - **password**: The username
+    - **Returns**: The user_id if it exists, or an error if not.
+    """
+    username = credentials["username"]
+    password = credentials["password"]
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    if not pwd_context.verify(password, user.hashed_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+    return {"user_id": user.id, "mesaage": "User logged in correctly"}
