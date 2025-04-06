9502efd25b3a8f23ecbf36c7298a1c21928c589a
Bernardo Koron <bernardokoron@gmail.com>
2025-04-06 11:52:02 +0200

    Documentation added

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ import uuid
 
 app = FastAPI()
 
+
 # Password Hashing
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -20,8 +21,15 @@ def get_db():
         db.close()
 
 # Register User
-@app.post("/register/")
+@app.post("/register/", tags=["Authentication"])
 def register(username: str, password: str, db: Session = Depends(get_db)):
+    """
+    Register a new user.
+
+    - **username**: The desired username (must be unique).
+    - **password**: The password.
+    - **Returns**: A success message if registration is successful.
+    """
     hashed_password = pwd_context.hash(password)
     user = User(username=username, hashed_password=hashed_password)
     db.add(user)
@@ -29,9 +37,33 @@ def register(username: str, password: str, db: Session = Depends(get_db)):
     db.refresh(user)
     return {"message": "User created successfully"}
 
+
+# Get user id
+@app.get("/get_user_id/", tags=["Authentication"])
+def get_user_id(username: str, db: Session = Depends(get_db)):
+    """
+    Gets user_id
+
+    - **username**: The username
+    - **Returns**: The user_id if it exists, or an error if not.
+    """
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return {"user_id": user.id}
+
+
 # Upload CSV File
-@app.post("/upload/")
-async def upload_file(file: UploadFile = File(...), username: str = "", password: str = "", db: Session = Depends(get_db)):
+@app.post("/upload/",tags=["File processing"])
+def upload_file(file: UploadFile = File(...), username: str = "", password: str = "", db: Session = Depends(get_db)):
+    """
+    Process a .csv file
+
+    - **file**: The .csv file to be processed.
+    - **username**: From the user uploading the file.
+    - **password**: From the user uploading the file.
+    - **Returns**: A success message if file processing is successful.
+    """
     user = db.query(User).filter(User.username == username).first()
 
     if not user or not pwd_context.verify(password, user.hashed_password):
